3d9467468552193536714cf89fda3bd37e3bda05
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2024-06-29 16:44:45 +0100

    Media queries added, hamburger added.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navList = document.getElementById("nav-list");
+
+  hamburger.addEventListener("click", function () {
+    navList.classList.toggle("active");
+  });
+});
+
 const typewriter = new Typewriter("#typewriter", {
   autoStart: true,
   delay: 80,
