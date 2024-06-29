bcab092f36ebb528a924fbc75b3836fb9babf883
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2024-06-29 17:01:36 +0100

    Change hamburger colour when active

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.getElementById("hamburger");
-  const navList = document.getElementById("nav-list");
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
 
-  hamburger.addEventListener("click", function () {
-    navList.classList.toggle("active");
-  });
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  hamburger.classList.toggle("active");
 });
 
 const typewriter = new Typewriter("#typewriter", {
